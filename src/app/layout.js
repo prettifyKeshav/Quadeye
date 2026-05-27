ce511e639a496ef3a159526ec347c3616a4c1add
Keshav Sharma <keshavsharma.prettifycreative@gmail.com>
2026-05-27 17:55:43 +0530

    added font graphic

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,45 @@
-// import "./global.css";
+// app/layout.tsx
 import MainTemplates from "@/components/templates/MainTemplates";
-import "@/uploads/styles/header/header.css"
-import { Dela_Gothic_One } from 'next/font/google'
+import "@/uploads/styles/header/header.css";
+import { Dela_Gothic_One } from 'next/font/google';
+import localFont from 'next/font/local';
+
+const graphik = localFont({
+  src: [
+    {
+      path: "../../public/assets/Graphik/Graphik-Regular-Trial.otf",
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/assets/Graphik/Graphik-Semibold-Trial.otf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../../public/assets/Graphik/Graphik-Bold-Trial.otf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-graphik',
+});
 
 const delaGothic = Dela_Gothic_One({
   subsets: ['latin'],
   weight: '400',
-})
+  variable: '--font-dela-gothic',
+});
 
 export const metadata = {
   title: "Quadeye",
-  description: "India’s Leading Proprietary Trading Firm",
+  description: "India's Leading Proprietary Trading Firm",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-scroll-behavior="smooth">
-      <body className="antialiased" >
+    <html lang="en" className={`${graphik.variable} ${delaGothic.variable}`}>
+      <body className="antialiased">
         <MainTemplates>
           {children}
         </MainTemplates>
