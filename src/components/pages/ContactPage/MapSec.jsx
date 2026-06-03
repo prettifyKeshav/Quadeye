6e57d3aedf01e1740b331eeffb00a781ad403076
Keshav Sharma <keshavsharma.prettifycreative@gmail.com>
2026-06-03 14:05:05 +0530

    contact us page completed

---
+++ src/components/pages/ContactPage/MapSec.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const MapSec = () => {
+    return (
+        <>
+            <section>
+                <div className="contact-secA" id='contact_secA'>
+                    <div className="container">
+                        <ul className="tabs">
+                            <li>Gurugram</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default MapSec
--- src/components/pages/ContactPage/index.jsx
+++ src/components/pages/ContactPage/index.jsx
@@ -6,6 +6,7 @@ import InvestorGrievance from './InvestorGrievance'
 import RespectiveExchanges from './RespectiveExchanges'
 import BankAccountDisclosure from './BankAccountDisclosure'
 import QuickLinks from './QuickLinks'
+import MapSec from './MapSec'
 
 const ContactPage = () => {
     return (
@@ -15,14 +16,15 @@ const ContactPage = () => {
                 imgSrc="/assets/images/contact/contact-banner.png"
                 imgHeight="626"
                 imgWidth="1028"
-                bannerClassName="career-banner"
+                bannerClassName="contact-banner"
                 heading="Reach Out and Let’s Build Smarter Strategies"
                 description="We’re here to answer your questions and discuss how our data-driven strategies can support your goals."
-                slideTo="#career_secA"
+                slideTo="#contact_secA"
                 isLinkAvalible
                 linkHref="#contact_sec"
                 linkText="get in touch"
             />
+            <MapSec />
             <InvestorGrievance />
             <RespectiveExchanges />
             <BankAccountDisclosure />
